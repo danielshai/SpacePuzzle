248e6f1011b0aa8bcd9bdf10bd0f8a1e7a1b263f
Victor Karlsson <victorc@student.chalmers.se>
2013-12-10 10:56:42 +0100

    Added star animations

---
+++ SpacePuzzle/SpacePuzzle/StarOnTile.h
@@ -0,0 +1,62 @@
+// ---------------------------------------
+// Sprite definitions for StarOnTile
+// Generated with TexturePacker 3.2.1
+//
+// http://www.codeandweb.com/texturepacker
+// ---------------------------------------
+
+#ifndef __STARONTILE_ATLAS__
+#define __STARONTILE_ATLAS__
+
+// ------------------------
+// name of the atlas bundle
+// ------------------------
+#define STARONTILE_ATLAS_NAME @"StarOnTile"
+
+// ------------
+// sprite names
+// ------------
+#define STARONTILE_SPR_00 @"00"
+#define STARONTILE_SPR_01 @"01"
+#define STARONTILE_SPR_02 @"02"
+#define STARONTILE_SPR_03 @"03"
+#define STARONTILE_SPR_04 @"04"
+#define STARONTILE_SPR_05 @"05"
+#define STARONTILE_SPR_06 @"06"
+#define STARONTILE_SPR_07 @"07"
+#define STARONTILE_SPR_08 @"08"
+#define STARONTILE_SPR_09 @"09"
+#define STARONTILE_SPR_10 @"10"
+#define STARONTILE_SPR_11 @"11"
+#define STARONTILE_SPR_12 @"12"
+#define STARONTILE_SPR_13 @"13"
+#define STARONTILE_SPR_14 @"14"
+#define STARONTILE_SPR_15 @"15"
+#define STARONTILE_SPR_16 @"16"
+
+// --------
+// textures
+// --------
+#define STARONTILE_TEX_00 [SKTexture textureWithImageNamed:@"00"]
+#define STARONTILE_TEX_01 [SKTexture textureWithImageNamed:@"01"]
+#define STARONTILE_TEX_02 [SKTexture textureWithImageNamed:@"02"]
+#define STARONTILE_TEX_03 [SKTexture textureWithImageNamed:@"03"]
+#define STARONTILE_TEX_04 [SKTexture textureWithImageNamed:@"04"]
+#define STARONTILE_TEX_05 [SKTexture textureWithImageNamed:@"05"]
+#define STARONTILE_TEX_06 [SKTexture textureWithImageNamed:@"06"]
+#define STARONTILE_TEX_07 [SKTexture textureWithImageNamed:@"07"]
+#define STARONTILE_TEX_08 [SKTexture textureWithImageNamed:@"08"]
+#define STARONTILE_TEX_09 [SKTexture textureWithImageNamed:@"09"]
+#define STARONTILE_TEX_10 [SKTexture textureWithImageNamed:@"10"]
+#define STARONTILE_TEX_11 [SKTexture textureWithImageNamed:@"11"]
+#define STARONTILE_TEX_12 [SKTexture textureWithImageNamed:@"12"]
+#define STARONTILE_TEX_13 [SKTexture textureWithImageNamed:@"13"]
+#define STARONTILE_TEX_14 [SKTexture textureWithImageNamed:@"14"]
+#define STARONTILE_TEX_15 [SKTexture textureWithImageNamed:@"15"]
+#define STARONTILE_TEX_16 [SKTexture textureWithImageNamed:@"16"]
+
+// ----------
+// animations
+// ----------
+
+#endif // __STARONTILE_ATLAS__
---
+++ SpacePuzzle/SpacePuzzle/StarTaken.h
@@ -0,0 +1,205 @@
+// ---------------------------------------
+// Sprite definitions for StarTaken
+// Generated with TexturePacker 3.2.1
+//
+// http://www.codeandweb.com/texturepacker
+// ---------------------------------------
+
+#ifndef __STARTAKEN_ATLAS__
+#define __STARTAKEN_ATLAS__
+
+// ------------------------
+// name of the atlas bundle
+// ------------------------
+#define STARTAKEN_ATLAS_NAME @"StarTaken"
+
+// ------------
+// sprite names
+// ------------
+#define STARTAKEN_SPR_STARTAKEN01 @"startaken01"
+#define STARTAKEN_SPR_STARTAKEN02 @"startaken02"
+#define STARTAKEN_SPR_STARTAKEN03 @"startaken03"
+#define STARTAKEN_SPR_STARTAKEN04 @"startaken04"
+#define STARTAKEN_SPR_STARTAKEN05 @"startaken05"
+#define STARTAKEN_SPR_STARTAKEN06 @"startaken06"
+#define STARTAKEN_SPR_STARTAKEN07 @"startaken07"
+#define STARTAKEN_SPR_STARTAKEN08 @"startaken08"
+#define STARTAKEN_SPR_STARTAKEN09 @"startaken09"
+#define STARTAKEN_SPR_STARTAKEN10 @"startaken10"
+#define STARTAKEN_SPR_STARTAKEN11 @"startaken11"
+#define STARTAKEN_SPR_STARTAKEN12 @"startaken12"
+#define STARTAKEN_SPR_STARTAKEN13 @"startaken13"
+#define STARTAKEN_SPR_STARTAKEN14 @"startaken14"
+#define STARTAKEN_SPR_STARTAKEN15 @"startaken15"
+#define STARTAKEN_SPR_STARTAKEN16 @"startaken16"
+#define STARTAKEN_SPR_STARTAKEN17 @"startaken17"
+#define STARTAKEN_SPR_STARTAKEN18 @"startaken18"
+#define STARTAKEN_SPR_STARTAKEN19 @"startaken19"
+#define STARTAKEN_SPR_STARTAKEN20 @"startaken20"
+#define STARTAKEN_SPR_STARTAKEN21 @"startaken21"
+#define STARTAKEN_SPR_STARTAKEN22 @"startaken22"
+#define STARTAKEN_SPR_STARTAKEN23 @"startaken23"
+#define STARTAKEN_SPR_STARTAKEN24 @"startaken24"
+#define STARTAKEN_SPR_STARTAKEN25 @"startaken25"
+#define STARTAKEN_SPR_STARTAKEN26 @"startaken26"
+#define STARTAKEN_SPR_STARTAKEN27 @"startaken27"
+#define STARTAKEN_SPR_STARTAKEN28 @"startaken28"
+#define STARTAKEN_SPR_STARTAKEN29 @"startaken29"
+#define STARTAKEN_SPR_STARTAKEN30 @"startaken30"
+#define STARTAKEN_SPR_STARTAKEN31 @"startaken31"
+#define STARTAKEN_SPR_STARTAKEN32 @"startaken32"
+#define STARTAKEN_SPR_STARTAKEN33 @"startaken33"
+#define STARTAKEN_SPR_STARTAKEN34 @"startaken34"
+#define STARTAKEN_SPR_STARTAKEN35 @"startaken35"
+#define STARTAKEN_SPR_STARTAKEN36 @"startaken36"
+#define STARTAKEN_SPR_STARTAKEN37 @"startaken37"
+#define STARTAKEN_SPR_STARTAKEN38 @"startaken38"
+#define STARTAKEN_SPR_STARTAKEN39 @"startaken39"
+#define STARTAKEN_SPR_STARTAKEN40 @"startaken40"
+#define STARTAKEN_SPR_STARTAKEN41 @"startaken41"
+#define STARTAKEN_SPR_STARTAKEN42 @"startaken42"
+#define STARTAKEN_SPR_STARTAKEN43 @"startaken43"
+#define STARTAKEN_SPR_STARTAKEN44 @"startaken44"
+#define STARTAKEN_SPR_STARTAKEN45 @"startaken45"
+#define STARTAKEN_SPR_STARTAKEN46 @"startaken46"
+#define STARTAKEN_SPR_STARTAKEN47 @"startaken47"
+#define STARTAKEN_SPR_STARTAKEN48 @"startaken48"
+#define STARTAKEN_SPR_STARTAKEN49 @"startaken49"
+#define STARTAKEN_SPR_STARTAKEN50 @"startaken50"
+#define STARTAKEN_SPR_STARTAKEN51 @"startaken51"
+#define STARTAKEN_SPR_STARTAKEN52 @"startaken52"
+#define STARTAKEN_SPR_STARTAKEN53 @"startaken53"
+#define STARTAKEN_SPR_STARTAKEN54 @"startaken54"
+#define STARTAKEN_SPR_STARTAKEN55 @"startaken55"
+#define STARTAKEN_SPR_STARTAKEN56 @"startaken56"
+#define STARTAKEN_SPR_STARTAKEN57 @"startaken57"
+#define STARTAKEN_SPR_STARTAKEN58 @"startaken58"
+
+// --------
+// textures
+// --------
+#define STARTAKEN_TEX_STARTAKEN01 [SKTexture textureWithImageNamed:@"startaken01"]
+#define STARTAKEN_TEX_STARTAKEN02 [SKTexture textureWithImageNamed:@"startaken02"]
+#define STARTAKEN_TEX_STARTAKEN03 [SKTexture textureWithImageNamed:@"startaken03"]
+#define STARTAKEN_TEX_STARTAKEN04 [SKTexture textureWithImageNamed:@"startaken04"]
+#define STARTAKEN_TEX_STARTAKEN05 [SKTexture textureWithImageNamed:@"startaken05"]
+#define STARTAKEN_TEX_STARTAKEN06 [SKTexture textureWithImageNamed:@"startaken06"]
+#define STARTAKEN_TEX_STARTAKEN07 [SKTexture textureWithImageNamed:@"startaken07"]
+#define STARTAKEN_TEX_STARTAKEN08 [SKTexture textureWithImageNamed:@"startaken08"]
+#define STARTAKEN_TEX_STARTAKEN09 [SKTexture textureWithImageNamed:@"startaken09"]
+#define STARTAKEN_TEX_STARTAKEN10 [SKTexture textureWithImageNamed:@"startaken10"]
+#define STARTAKEN_TEX_STARTAKEN11 [SKTexture textureWithImageNamed:@"startaken11"]
+#define STARTAKEN_TEX_STARTAKEN12 [SKTexture textureWithImageNamed:@"startaken12"]
+#define STARTAKEN_TEX_STARTAKEN13 [SKTexture textureWithImageNamed:@"startaken13"]
+#define STARTAKEN_TEX_STARTAKEN14 [SKTexture textureWithImageNamed:@"startaken14"]
+#define STARTAKEN_TEX_STARTAKEN15 [SKTexture textureWithImageNamed:@"startaken15"]
+#define STARTAKEN_TEX_STARTAKEN16 [SKTexture textureWithImageNamed:@"startaken16"]
+#define STARTAKEN_TEX_STARTAKEN17 [SKTexture textureWithImageNamed:@"startaken17"]
+#define STARTAKEN_TEX_STARTAKEN18 [SKTexture textureWithImageNamed:@"startaken18"]
+#define STARTAKEN_TEX_STARTAKEN19 [SKTexture textureWithImageNamed:@"startaken19"]
+#define STARTAKEN_TEX_STARTAKEN20 [SKTexture textureWithImageNamed:@"startaken20"]
+#define STARTAKEN_TEX_STARTAKEN21 [SKTexture textureWithImageNamed:@"startaken21"]
+#define STARTAKEN_TEX_STARTAKEN22 [SKTexture textureWithImageNamed:@"startaken22"]
+#define STARTAKEN_TEX_STARTAKEN23 [SKTexture textureWithImageNamed:@"startaken23"]
+#define STARTAKEN_TEX_STARTAKEN24 [SKTexture textureWithImageNamed:@"startaken24"]
+#define STARTAKEN_TEX_STARTAKEN25 [SKTexture textureWithImageNamed:@"startaken25"]
+#define STARTAKEN_TEX_STARTAKEN26 [SKTexture textureWithImageNamed:@"startaken26"]
+#define STARTAKEN_TEX_STARTAKEN27 [SKTexture textureWithImageNamed:@"startaken27"]
+#define STARTAKEN_TEX_STARTAKEN28 [SKTexture textureWithImageNamed:@"startaken28"]
+#define STARTAKEN_TEX_STARTAKEN29 [SKTexture textureWithImageNamed:@"startaken29"]
+#define STARTAKEN_TEX_STARTAKEN30 [SKTexture textureWithImageNamed:@"startaken30"]
+#define STARTAKEN_TEX_STARTAKEN31 [SKTexture textureWithImageNamed:@"startaken31"]
+#define STARTAKEN_TEX_STARTAKEN32 [SKTexture textureWithImageNamed:@"startaken32"]
+#define STARTAKEN_TEX_STARTAKEN33 [SKTexture textureWithImageNamed:@"startaken33"]
+#define STARTAKEN_TEX_STARTAKEN34 [SKTexture textureWithImageNamed:@"startaken34"]
+#define STARTAKEN_TEX_STARTAKEN35 [SKTexture textureWithImageNamed:@"startaken35"]
+#define STARTAKEN_TEX_STARTAKEN36 [SKTexture textureWithImageNamed:@"startaken36"]
+#define STARTAKEN_TEX_STARTAKEN37 [SKTexture textureWithImageNamed:@"startaken37"]
+#define STARTAKEN_TEX_STARTAKEN38 [SKTexture textureWithImageNamed:@"startaken38"]
+#define STARTAKEN_TEX_STARTAKEN39 [SKTexture textureWithImageNamed:@"startaken39"]
+#define STARTAKEN_TEX_STARTAKEN40 [SKTexture textureWithImageNamed:@"startaken40"]
+#define STARTAKEN_TEX_STARTAKEN41 [SKTexture textureWithImageNamed:@"startaken41"]
+#define STARTAKEN_TEX_STARTAKEN42 [SKTexture textureWithImageNamed:@"startaken42"]
+#define STARTAKEN_TEX_STARTAKEN43 [SKTexture textureWithImageNamed:@"startaken43"]
+#define STARTAKEN_TEX_STARTAKEN44 [SKTexture textureWithImageNamed:@"startaken44"]
+#define STARTAKEN_TEX_STARTAKEN45 [SKTexture textureWithImageNamed:@"startaken45"]
+#define STARTAKEN_TEX_STARTAKEN46 [SKTexture textureWithImageNamed:@"startaken46"]
+#define STARTAKEN_TEX_STARTAKEN47 [SKTexture textureWithImageNamed:@"startaken47"]
+#define STARTAKEN_TEX_STARTAKEN48 [SKTexture textureWithImageNamed:@"startaken48"]
+#define STARTAKEN_TEX_STARTAKEN49 [SKTexture textureWithImageNamed:@"startaken49"]
+#define STARTAKEN_TEX_STARTAKEN50 [SKTexture textureWithImageNamed:@"startaken50"]
+#define STARTAKEN_TEX_STARTAKEN51 [SKTexture textureWithImageNamed:@"startaken51"]
+#define STARTAKEN_TEX_STARTAKEN52 [SKTexture textureWithImageNamed:@"startaken52"]
+#define STARTAKEN_TEX_STARTAKEN53 [SKTexture textureWithImageNamed:@"startaken53"]
+#define STARTAKEN_TEX_STARTAKEN54 [SKTexture textureWithImageNamed:@"startaken54"]
+#define STARTAKEN_TEX_STARTAKEN55 [SKTexture textureWithImageNamed:@"startaken55"]
+#define STARTAKEN_TEX_STARTAKEN56 [SKTexture textureWithImageNamed:@"startaken56"]
+#define STARTAKEN_TEX_STARTAKEN57 [SKTexture textureWithImageNamed:@"startaken57"]
+#define STARTAKEN_TEX_STARTAKEN58 [SKTexture textureWithImageNamed:@"startaken58"]
+
+// ----------
+// animations
+// ----------
+#define STARTAKEN_ANIM_STARTAKEN @[ \
+        [SKTexture textureWithImageNamed:@"startaken01"], \
+        [SKTexture textureWithImageNamed:@"startaken02"], \
+        [SKTexture textureWithImageNamed:@"startaken03"], \
+        [SKTexture textureWithImageNamed:@"startaken04"], \
+        [SKTexture textureWithImageNamed:@"startaken05"], \
+        [SKTexture textureWithImageNamed:@"startaken06"], \
+        [SKTexture textureWithImageNamed:@"startaken07"], \
+        [SKTexture textureWithImageNamed:@"startaken08"], \
+        [SKTexture textureWithImageNamed:@"startaken09"], \
+        [SKTexture textureWithImageNamed:@"startaken10"], \
+        [SKTexture textureWithImageNamed:@"startaken11"], \
+        [SKTexture textureWithImageNamed:@"startaken12"], \
+        [SKTexture textureWithImageNamed:@"startaken13"], \
+        [SKTexture textureWithImageNamed:@"startaken14"], \
+        [SKTexture textureWithImageNamed:@"startaken15"], \
+        [SKTexture textureWithImageNamed:@"startaken16"], \
+        [SKTexture textureWithImageNamed:@"startaken17"], \
+        [SKTexture textureWithImageNamed:@"startaken18"], \
+        [SKTexture textureWithImageNamed:@"startaken19"], \
+        [SKTexture textureWithImageNamed:@"startaken20"], \
+        [SKTexture textureWithImageNamed:@"startaken21"], \
+        [SKTexture textureWithImageNamed:@"startaken22"], \
+        [SKTexture textureWithImageNamed:@"startaken23"], \
+        [SKTexture textureWithImageNamed:@"startaken24"], \
+        [SKTexture textureWithImageNamed:@"startaken25"], \
+        [SKTexture textureWithImageNamed:@"startaken26"], \
+        [SKTexture textureWithImageNamed:@"startaken27"], \
+        [SKTexture textureWithImageNamed:@"startaken28"], \
+        [SKTexture textureWithImageNamed:@"startaken29"], \
+        [SKTexture textureWithImageNamed:@"startaken30"], \
+        [SKTexture textureWithImageNamed:@"startaken31"], \
+        [SKTexture textureWithImageNamed:@"startaken32"], \
+        [SKTexture textureWithImageNamed:@"startaken33"], \
+        [SKTexture textureWithImageNamed:@"startaken34"], \
+        [SKTexture textureWithImageNamed:@"startaken35"], \
+        [SKTexture textureWithImageNamed:@"startaken36"], \
+        [SKTexture textureWithImageNamed:@"startaken37"], \
+        [SKTexture textureWithImageNamed:@"startaken38"], \
+        [SKTexture textureWithImageNamed:@"startaken39"], \
+        [SKTexture textureWithImageNamed:@"startaken40"], \
+        [SKTexture textureWithImageNamed:@"startaken41"], \
+        [SKTexture textureWithImageNamed:@"startaken42"], \
+        [SKTexture textureWithImageNamed:@"startaken43"], \
+        [SKTexture textureWithImageNamed:@"startaken44"], \
+        [SKTexture textureWithImageNamed:@"startaken45"], \
+        [SKTexture textureWithImageNamed:@"startaken46"], \
+        [SKTexture textureWithImageNamed:@"startaken47"], \
+        [SKTexture textureWithImageNamed:@"startaken48"], \
+        [SKTexture textureWithImageNamed:@"startaken49"], \
+        [SKTexture textureWithImageNamed:@"startaken50"], \
+        [SKTexture textureWithImageNamed:@"startaken51"], \
+        [SKTexture textureWithImageNamed:@"startaken52"], \
+        [SKTexture textureWithImageNamed:@"startaken53"], \
+        [SKTexture textureWithImageNamed:@"startaken54"], \
+        [SKTexture textureWithImageNamed:@"startaken55"], \
+        [SKTexture textureWithImageNamed:@"startaken56"], \
+        [SKTexture textureWithImageNamed:@"startaken57"], \
+        [SKTexture textureWithImageNamed:@"startaken58"]  \
+    ]
+
+
+#endif // __STARTAKEN_ATLAS__
