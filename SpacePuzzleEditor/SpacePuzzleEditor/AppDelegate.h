ad174dda1cc4a754a7f1ea39f1dae86172f6d311
Victor Karlsson <victorc@student.chalmers.se>
2014-02-03 09:44:42 +0100

    Added comment

--- SpacePuzzleEditor/SpacePuzzleEditor/AppDelegate.h
+++ SpacePuzzleEditor/SpacePuzzleEditor/AppDelegate.h
@@ -5,11 +5,13 @@
 //  TODO:
 //    - Fixa macros med klassnamn.
 //    - Klicka på element för att selecta och då kunna t.ex. ta bort?
+//    - Hur edit path. Det går ju nu, men musen är på fel ställe så at säga (bara flytta den till rätt?).
+//      Dock går det inte genom sista pointen i pathen, fixa det.
 //    - Städa: Synka alltid viewn (BoardScene) utifrån datamodellen. Alltså när en |editABoardItem| sker,
 //             säg bara till AppDelegate, som i sin tur uppdaterar datamodellen och efter det uppdaterar
 //             BoardScene, typ en metod |updateView|
 //             Inga notifications, ist refs i view och controller.
-//    - Lever, moving platform, path (alla elements). Bridge horizontal/vertical?
+//    - Bridge horizontal/vertical?
 #import <Cocoa/Cocoa.h>
 #import <SpriteKit/SpriteKit.h>
 #import "Board.h"
